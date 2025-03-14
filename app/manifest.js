d88b31ef8c0fc4aa683740c67bed854f668db856
egarrisxn <egarrisxn@gmail.com>
2025-03-14 00:07:32 -0400

    more metadata and image optimization

--- app/manifest.js
+++ app/manifest.js
@@ -3,8 +3,10 @@ export default function manifest() {
     name: "egxworld",
     short_name: "egxo",
     description: "Full Stack Developer. Digital Product Designer.",
+    id: "/",
     start_url: "/",
     display: "standalone",
+    orientation: "any",
     theme_color: "#ffffff",
     background_color: "#ffffff",
     icons: [
@@ -31,6 +33,19 @@ export default function manifest() {
         sizes: "192x192",
         type: "image/png",
         purpose: "maskable",
+      },
+    ],
+    screenshots: [
+      {
+        src: "/screenshots/desktop.png",
+        sizes: "1920x1080",
+        type: "image/png",
+        form_factor: "wide",
+      },
+      {
+        src: "/screenshots/mobile.png",
+        sizes: "750x1334",
+        type: "image/png",
       },
     ],
   };
