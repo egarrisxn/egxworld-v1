7649210e9b90a88f0320806ab51de37b3d4759b5
egarrisxn <egarrisxn@gmail.com>
2025-04-12 13:58:21 -0400

    stuff

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -7,7 +7,7 @@ export function Footer() {
       <p className="shadow-foreground bg-linear-to-tl from-zinc-400 via-zinc-600 to-zinc-900 bg-clip-text text-transparent drop-shadow-2xl sm:text-base lg:text-lg xl:text-xl 2xl:text-3xl dark:from-zinc-200 dark:via-zinc-400 dark:to-zinc-700">
         <span className="text-foreground">EG</span>|{date}
       </p>
-      <section className="gap-1. shadow-foreground flex text-sm drop-shadow-2xl md:text-base lg:text-lg xl:text-xl 2xl:gap-2 2xl:text-2xl">
+      <section className="shadow-foreground flex gap-1 text-sm drop-shadow-2xl md:text-base lg:text-lg xl:text-xl 2xl:gap-2 2xl:text-2xl">
         <SocialList />
       </section>
     </footer>
